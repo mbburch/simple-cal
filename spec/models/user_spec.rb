1038fa4c76cd1429cfac7c31fcb30b9cfb5ea931
MB Burch <marybethburch@gmail.com>
2017-09-24 15:39:25 -0600

    git commit —amend

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe 'user creation' do
-    it 'is valid with required attributes' do
+  describe "user creation" do
+    it "is valid with required attributes" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it 'is invalid without email' do
-      user = build(:user, email: '')
+    it "is invalid without email" do
+      user = build(:user, email: "")
       expect(user).not_to be_valid
     end
 
-    it 'is invalid with incorrect password_confirmation' do
-      user = build(:user, password_confirmation: 'passWord')
+    it "is invalid with incorrect password_confirmation" do
+      user = build(:user, password_confirmation: "passWord")
       expect(user).not_to be_valid
     end
   end
