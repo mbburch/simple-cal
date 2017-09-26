7c43ab49659038fadb0abf4f2df43adf720bc9ac
MB Burch <marybethburch@gmail.com>
2017-09-25 21:03:41 -0600

    Customize devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   has_many :events, dependent: :destroy
   has_many :categories, dependent: :destroy
